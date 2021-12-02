91bc18178394163fa74011ef8818fdfc75cf8ff5
Daniel Klockars <dclock90@gmail.com>
2021-12-02 14:38:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Design_Patterns_Assignment;
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var decoratorRun = new DecoratorRun();
+            var strategyRun = new StrategyRun();
+
+            decoratorRun.Run();
+            //Repository.Run();
+            //strategyRun.Run();
+            //Observer.Run();
+        }     
+    }
+}
